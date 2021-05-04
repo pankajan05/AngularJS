b8618e56ccb5d4c28641cbae04c1c8cec87fb8dc
pankajan <pankajan05@gmail.com>
2021-05-04 22:11:51 +0530

    learn the ngfor and the two-way binding

--- js/Module.js
+++ js/Module.js
@@ -1,4 +1,11 @@
 var myApp = angular.module('Myapp', [])
     .controller('firstController', function ($scope) {
         $scope.name = "pankajan";
+
+        $scope.array = [
+            { name: "ayanibas", age: 23 },
+            { name: "iklid", age: 24 },
+            { name: "ahsunaj", age: 24 },
+            { name: "aknawiht", age: 25 },
+            { name: "akidnar", age: 24 }];
     });
