aa091d5251e5d27f9b214e16ab3bea29178209f6
pankajan <pankajan05@gmail.com>
2021-05-04 23:41:27 +0530

    learn about filter

--- js/Module.js
+++ js/Module.js
@@ -1,15 +1,37 @@
 var myApp = angular.module('Myapp', [])
+    .filter('gender', function () {
+        return function (gender) {
+            switch (gender) {
+                case 1:
+                    return "female";
+                case 2:
+                    return "male";
+                case 3:
+                    return "unknown";
+            }
+        }
+    })
     .controller('firstController', function ($scope) {
         $scope.name = "pankajan";
 
         $scope.array = [
-            { name: "ayanibas", age: 23 },
-            { name: "iklid", age: 24 },
-            { name: "ahsunaj", age: 24 },
-            { name: "aknawiht", age: 25 },
-            { name: "akidnar", age: 24 }];
+            { name: "ayanibas", age: 23, gender: 1},
+            { name: "iklid", age: 24, gender: 1 },
+            { name: "ahsunaj", age: 24, gender: 1 },
+            { name: "aknawiht", age: 25, gender: 1 },
+            { name: "akidnar", age: 24, gender: 1 }];
 
         $scope.searchthis = function(word){
             console.log(word);
+        }
+
+        $scope.search = function (item) {
+            console.log($scope.searchText);
+            if ($scope.searchText == undefined) {
+                return true;
+            }else
+            if (item.name.toLowerCase().indexOf($scope.searchText.toLowerCase()) != -1) {
+                return true;
+            }
         }
     });
